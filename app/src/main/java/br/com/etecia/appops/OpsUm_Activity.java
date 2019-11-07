9d99a5585a57f50d68a96140eabadea623e627fa
kevin <kevinorigami2013@gmail.com>
2019-11-06 21:58:05 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appops/OpsUm_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpsUm_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ops_um_layout);
+    }
+}
